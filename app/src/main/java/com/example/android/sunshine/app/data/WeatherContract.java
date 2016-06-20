86d3d19f24f6f2fc5f192f9e3995f7fad57394f3
Alicia Perez <pericia@gmail.com>
2016-06-19 20:43:05 -0400

    Uncommenting parts of the code that was provided in order to be able to execute the tests (also provided).

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -41,6 +41,17 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+        //setting to display corresponding image
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        //city name
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        //latitude
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+
+        //longitude
+        public static final String COLUMN_COORD_LONG = "coord_long";
     }
 
     /* Inner class that defines the table contents of the weather table */
